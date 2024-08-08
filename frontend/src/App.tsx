5122e238ed69d52d0eac178813a5e0d3f0b6fc1a
ledsouza <leandro.souza.159@gmail.com>
2024-08-08 12:00:45 -0300

    configurando frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <h2 className="flex justify-center text-center text-2xl">
+                Hello World!
+            </h2>
+        </>
+    );
+}
+
+export default App;
