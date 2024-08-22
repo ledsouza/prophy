c1aa7f8115f7b53238d2e4a7dec69fb5b5070ef0
ledsouza <leandro.souza.159@gmail.com>
2024-08-22 14:38:23 -0300

    add nav header

--- frontend/app/components/common/NavBar.tsx
+++ frontend/app/components/common/NavBar.tsx
@@ -1,7 +1,118 @@
-export default function NavBar() {
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { useLogoutMutation } from "@/redux/features/authApiSlice";
+import { logout as setLogout } from "@/redux/features/authSlice";
+
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+} from "@headlessui/react";
+
+import { NavLink } from "@/components/common";
+
+export default function Navbar() {
+    const router = useRouter();
+    const pathname = usePathname();
+    const dispatch = useAppDispatch();
+
+    const [logout] = useLogoutMutation();
+
+    const { isAuthenticated } = useAppSelector((state) => state.auth);
+
+    const handleLogout = () => {
+        logout()
+            .unwrap()
+            .then(() => {
+                dispatch(setLogout());
+                router.push("/auth/login");
+            });
+    };
+
+    const isSelected = (path: string) => (pathname === path ? true : false);
+
+    const authLinks = (isMobile: boolean) => (
+        <>
+            <NavLink
+                isSelected={isSelected("/dashboard")}
+                isMobile={isMobile}
+                href="/dashboard"
+            >
+                Dashboard
+            </NavLink>
+            <NavLink isMobile={isMobile} onClick={handleLogout}>
+                Logout
+            </NavLink>
+        </>
+    );
+
+    const guestLinks = (isMobile: boolean) => (
+        <>
+            <NavLink
+                isSelected={isSelected("/auth/login")}
+                isMobile={isMobile}
+                href="/auth/login"
+            >
+                Login
+            </NavLink>
+        </>
+    );
+
     return (
-        <nav>
-            <h1>Navbar</h1>
-        </nav>
+        <Disclosure as="nav" className="bg-gray-800">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                <DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">
+                                        Abri menu principal
+                                    </span>
+                                    {open ? (
+                                        <XMarkIcon
+                                            className="block h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    ) : (
+                                        <Bars3Icon
+                                            className="block h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    )}
+                                </DisclosureButton>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <NavLink href="/" isBanner>
+                                        Prophy
+                                    </NavLink>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {isAuthenticated
+                                            ? authLinks(false)
+                                            : guestLinks(false)}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <DisclosurePanel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {isAuthenticated
+                                ? authLinks(true)
+                                : guestLinks(true)}
+                        </div>
+                    </DisclosurePanel>
+                </>
+            )}
+        </Disclosure>
     );
 }
--- frontend/app/components/common/index.tsx
+++ frontend/app/components/common/index.tsx
@@ -1,3 +1,4 @@
 export { default as Footer } from "./Footer";
 export { default as NavBar } from "./NavBar";
+export { default as NavLink } from "./NavLink";
 export { default as Spinner } from "./Spinner";
