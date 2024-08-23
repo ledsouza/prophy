b92bf3253de823ec27fe2862a53a01faa711433a
ledsouza <leandro.souza.159@gmail.com>
2024-08-23 08:52:47 -0300

    adjusted auth cookie lifetime

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -20,7 +20,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             response.set_cookie(
                 'access',
                 access_token,
-                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
@@ -29,7 +29,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             response.set_cookie(
                 'refresh',
                 refresh_token,
-                max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
@@ -54,7 +54,7 @@ class CustomTokenRefreshView(TokenRefreshView):
             response.set_cookie(
                 'access',
                 access_token,
-                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
