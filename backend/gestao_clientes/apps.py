ab509c06086fbd46e2c0ca49a200528e0da7223d
ledsouza <leandro.souza.159@gmail.com>
2024-08-24 09:14:08 -0300

    update the verbose name of gestao_clientes app

--- backend/gestao_clientes/apps.py
+++ backend/gestao_clientes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GestaoClientesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gestao_clientes'
+    verbose_name = "Gestão de Clientes"
