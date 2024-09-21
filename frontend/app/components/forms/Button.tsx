a7c01666901ab793e116d0f27719625b41ab4dae
ledsouza <leandro.souza.159@gmail.com>
2024-09-21 09:55:36 -0300

    refactored Button classNames

--- frontend/app/components/forms/Button.tsx
+++ frontend/app/components/forms/Button.tsx
@@ -1,5 +1,6 @@
 import { ButtonHTMLAttributes } from "react";
 import { Spinner } from "@/components/common";
+import cn from "classnames";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     variant?: "primary" | "secondary" | "danger";
@@ -11,29 +12,27 @@ const Button = ({
     disabled,
     variant = "primary",
     isLoading = false,
+    className,
     ...props
 }: ButtonProps) => {
-    const getButtonClasses = (): string => {
-        const baseClasses =
-            "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ";
-
-        switch (variant) {
-            case "primary":
-                return `${baseClasses} bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-indigo-600`;
-            case "secondary":
-                return `${baseClasses} bg-gray-200 text-gray-800 hover:bg-gray-300 focus-visible:outline-gray-600`;
-            case "danger":
-                return `${baseClasses} bg-red-600 text-white hover:bg-red-500 focus-visible:outline-red-600`;
-            default:
-                return baseClasses;
+    const computedClassName = cn(
+        className,
+        "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+        {
+            "bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-indigo-600":
+                variant === "primary",
+            "bg-gray-200 text-gray-800 hover:bg-gray-300 focus-visible:outline-gray-600":
+                variant === "secondary",
+            "bg-red-600 text-white hover:bg-red-500 focus-visible:outline-red-600":
+                variant === "danger",
         }
-    };
+    );
 
     return (
         <div className="flex justify-center">
             <button
                 {...props}
-                className={getButtonClasses()}
+                className={computedClassName}
                 disabled={disabled || isLoading}
             >
                 {isLoading ? (
