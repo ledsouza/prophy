5a2228ccea37564a8498a87d1bba40bda9afbe9b
ledsouza <leandro.souza.159@gmail.com>
2024-09-22 15:02:21 -0300

    updated input for error validation

--- frontend/app/components/forms/Input.tsx
+++ frontend/app/components/forms/Input.tsx
@@ -1,17 +1,21 @@
 import { forwardRef, InputHTMLAttributes } from "react";
 import cn from "classnames";
 
-type Props = InputHTMLAttributes<HTMLInputElement> & {
+export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
     disabled?: boolean;
     errorMessage?: string;
 };
 
-const Input = forwardRef<HTMLInputElement, Props>(
-    ({ disabled = false, errorMessage, children, ...props }: Props, ref) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    (
+        { disabled = false, errorMessage, children, ...props }: InputProps,
+        ref
+    ) => {
         const inputClassName = cn(
             "block w-full rounded-md border-0 py-1.5 text-gray-primary shadow-md ring-1 ring-inset ring-primary placeholder:text-placeholder focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6",
             {
                 "ring-tertiary": disabled,
+                "bg-danger bg-opacity-5 ring-danger": errorMessage,
             }
         );
 
---
+++ frontend/stories/forms/Input.stories.ts
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Input } from "@/components/forms";
+import { InputProps } from "@/components/forms/Input";
+
+const meta: Meta<InputProps> = {
+    title: "Forms/Input",
+    tags: ["autodocs"],
+    component: Input,
+    argTypes: {
+        children: {
+            type: "string",
+        },
+        placeholder: {
+            type: "string",
+        },
+        disabled: {
+            type: "boolean",
+        },
+        errorMessage: {
+            type: "string",
+        },
+    },
+};
+
+export default meta;
+
+export const Primary: StoryObj<InputProps> = {
+    args: {
+        children: "Input",
+    },
+};
