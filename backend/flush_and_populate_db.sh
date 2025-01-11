96611e10ce5ed741aa51549840fd87ba255affa3
ledsouza <leandro.souza.159@gmail.com>
2025-01-11 11:06:09 -0300

    refactor: improved flush and populate db shell script

--- backend/flush_and_populate_db.sh
+++ backend/flush_and_populate_db.sh
@@ -3,5 +3,8 @@
 # Echo "yes" and flush the database
 echo -e "yes" | python manage.py flush
 
+# Remove all media files
+rm -rf media/equipments/photos/*
+
 # Run the populate command
 python manage.py populate
