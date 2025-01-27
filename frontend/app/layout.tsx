71bdbe969d12d0307fe6fb1e99b60b0c6564d701
ledsouza <leandro.souza.159@gmail.com>
2025-01-27 15:30:01 -0300

    feat: improved UI

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
                 <Provider>
                     <Setup />
                     <NavBar />
-                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-4">
                         {children}
                     </div>
                     <Footer />
