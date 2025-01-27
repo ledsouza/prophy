d9a7fb692436f09bbd340e8e7aed72e49eb91d42
ledsouza <leandro.souza.159@gmail.com>
2025-01-27 15:24:21 -0300

    feat: updated the footer

--- frontend/app/components/common/Footer.tsx
+++ frontend/app/components/common/Footer.tsx
@@ -1,12 +1,25 @@
+import { Typography } from "@/components/foundation";
+
 export default function Footer() {
     return (
         <footer className="bg-quaternary bg-opacity-10 h-16">
             <div className="h-full px-2">
-                <div className="flex items-center justify-center h-full">
-                    <p className="text-gray-secondary text-opacity-60 text-xs">
-                        &copy; {new Date().getFullYear()} Prophy, Inc. Todos os
+                <div className="flex items-center justify-around h-full opacity-60">
+                    <Typography
+                        element="p"
+                        size="sm"
+                        variant="secondary"
+                        className="bold"
+                    >
+                        MedPhys Hub
+                    </Typography>
+                    <Typography element="p" size="sm" variant="secondary">
+                        Desenvolvido por PhysCollab Stack para Prophy. Todos os
                         direitos reservados.
-                    </p>
+                    </Typography>
+                    <Typography element="p" size="sm" variant="secondary">
+                        &copy; {new Date().getFullYear()} PhysCollab Stack.
+                    </Typography>
                 </div>
             </div>
         </footer>
