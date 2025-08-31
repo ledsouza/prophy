b153248f9321045323ff62754b30d8d9606ba428
ledsouza <leandro.souza.159@gmail.com>
2025-08-31 16:17:29 -0300

    fix: inconsistency between Input and Textarea style

--- frontend/app/components/forms/Textarea.tsx
+++ frontend/app/components/forms/Textarea.tsx
@@ -1,21 +1,26 @@
-import { forwardRef, ReactNode } from "react";
 import {
     Textarea as HeadlessTextarea,
     TextareaProps as HeadlessTextareaProps,
 } from "@headlessui/react";
-import cn from "classnames";
+import clsx from "clsx";
+import { forwardRef, ReactNode } from "react";
 
-type TextareaProps = HeadlessTextareaProps & {
+type TextareaProps = Omit<HeadlessTextareaProps, "children"> & {
     errorMessage?: string;
+    children?: ReactNode;
 };
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     ({ errorMessage, children, disabled, ...props }: TextareaProps, ref) => {
-        const inputClassName = cn(
-            "block w-full mt-1 rounded-md border-0 text-gray-primary shadow-md ring-1 ring-inset placeholder:text-placeholder focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6",
+        const inputClassName = clsx(
+            "block w-full rounded-md border-0",
+            "text-gray-primary shadow-md",
+            "ring-1 ring-inset placeholder:text-placeholder focus:ring-2 focus:ring-inset focus:ring-primary",
+            "sm:text-sm sm:leading-6",
             {
                 "py-1.5 ring-tertiary": disabled,
                 "bg-danger bg-opacity-5 ring-danger": errorMessage,
+                "py-1.5 ring-primary": !errorMessage && !disabled,
             }
         );
 
@@ -24,22 +29,22 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
                 {children && (
                     <div className="flex justify-between align-center">
                         <label className="block text-sm font-medium leading-6 text-gray-primary">
-                            {children as ReactNode}
+                            {children}
                         </label>
                     </div>
                 )}
 
-                <HeadlessTextarea
-                    ref={ref}
-                    className={inputClassName}
-                    {...props}
-                />
+                <div className="mt-2">
+                    <HeadlessTextarea
+                        ref={ref}
+                        disabled={disabled}
+                        className={inputClassName}
+                        {...props}
+                    />
+                </div>
 
                 {errorMessage && (
-                    <div
-                        data-testid="validation-error"
-                        className="text-danger mt-1"
-                    >
+                    <div data-testid="validation-error" className="text-danger mt-1">
                         {errorMessage}
                     </div>
                 )}
@@ -48,4 +53,5 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     }
 );
 
+Textarea.displayName = "Textarea";
 export default Textarea;
