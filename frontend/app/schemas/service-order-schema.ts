7996a75cb3ac8b8d4ac7756d578161bd8e41d875
ledsouza <leandro.souza.159@gmail.com>
2025-09-14 14:01:09 -0300

    feat: create service order schema

---
+++ frontend/app/schemas/service-order-schema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const serviceOrderSchema = z.object({
+    subject: z
+        .string()
+        .min(1, { message: "Assunto é obrigatório." })
+        .max(50, { message: "Assunto deve ter no máximo 50 caracteres." })
+        .transform((value) => value?.trim()),
+    description: z
+        .string()
+        .min(1, { message: "Descrição é obrigatória." })
+        .transform((value) => value?.trim()),
+    conclusion: z
+        .string()
+        .min(1, { message: "Conclusão é obrigatória." })
+        .transform((value) => value?.trim()),
+    equipments: z.array(z.number()).default([]),
+});
+
+export default serviceOrderSchema;
