d4b3ae1a9f96a4b540a62f11bd2c515ddd6d3866
ledsouza <leandro.souza.159@gmail.com>
2026-01-25 13:39:13 -0300

    feat(users): add managed user password reset email

--- backend/users/email.py
+++ backend/users/email.py
@@ -3,3 +3,7 @@ from djoser.email import PasswordResetEmail
 
 class UnitManagerPasswordResetEmail(PasswordResetEmail):
     template_name = "email/password_reset.html"
+
+
+class ManagedUserPasswordResetEmail(PasswordResetEmail):
+    template_name = "email/managed-user-password-reset.html"
